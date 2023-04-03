b9a32a19cc7dc499895cd8fe25c3e2bf4fb6c10e
Anne-Lie Smith <smith.annelie@outlook.com>
2023-04-03 18:23:41 +0000

    Add different type of players

--- run.py
+++ run.py
@@ -1,2 +1,27 @@
-print("hello world")
+import math
+import random
+
+# Different type of players
+class Player:
+    # Letter is x or o 
+    delf.letter = letter
+
+
+    # All players get their next move given a game
+    def get_move(self,game):
+        pass
+
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+        def get_move(self, game):
+            pass
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+        def get_move(self, game):
+            pass
 
