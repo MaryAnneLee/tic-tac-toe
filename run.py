81fdce2d1319991f3382324997f87fa6fef8e755
Anne-Lie Smith <smith.annelie@outlook.com>
2023-04-03 18:43:42 +0000

    Add game board and available moves

--- run.py
+++ run.py
@@ -25,3 +25,24 @@ class HumanPlayer(Player):
         def get_move(self, game):
             pass
 
+# The board - a single list to rep 3x3 
+class TicTacToe:
+    def __init__(self):
+        self.board = [' ' for _ in range(9)] 
+        self.current_winner = None # Keeping track of the winner
+
+    def print_board(self):
+        #getting the rows
+        for row in [self.board[i*3:(i+1)*3] for i in range(3)]:
+            print('|' + '|'.join(row) + '|')
+
+    @staticmetod
+    def print_board_nums():
+        # 0 | 1 | 2 etc - what number corresponds to what box
+        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)]
+        for row in number_board:
+            print('|' + '|'.join(row) + '|')
+
+    # Available moves
+    def available_moves(self):
+        return [i for i, spot in enumerate(self.board) if spot == ' ']
